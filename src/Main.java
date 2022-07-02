3116c44d98e967846c03fb68eff4ea6d5cce2611
G87388Dhfu38fj <exploitalts986@gmail.com>
2022-07-02 16:57:05 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(500, 500);
+        frame.setResizable(false);
+        frame.setTitle("Useless Frame");
+        
+    }
+}
